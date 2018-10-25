3671a2bf657b50f3046e5b124dad267605f2ea88
Paulo Andre de Oliveira Carneiro <paulo_carneiro@usp.br>
2018-10-25 18:49:25 -0200

    Primeiro algoritmo programacao dinamica

---
+++ PD/FibonacciDP.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+#define pb push_back
+
+vector<int> memo;
+
+//Complexidade numero de subproblemas . tempo de cada subproblema
+//portanto no final fibDp eh O(n) de tempo e O(n) de memoria
+int fibDp(int n){
+	if(memo[n] != -1) return memo[n];
+	if(n <=2) return 1;
+	else{
+		memo[n] = fibDp(n-1) + fibDp(n-2);
+		return memo[n]; 
+	}
+}
+//fib recursivo sem DP , complixedade exponencial
+int fib(int n){
+	if(n <= 2) return 1;
+	else return fib(n-1) + fib(n-2);
+}
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	for(int i=0; i<=n;i++)
+		memo.pb(-1);
+	printf("%d\n", fibDp(n));
+	return 0;
+}
